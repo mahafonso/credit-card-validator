8eed029b2f2e60be74b9d9bd32aa354ea6ad85a5
Marcela Afonso <marcela.bafonso3@gmail.com>
2020-04-27 20:00:08 -0300

    Verify brand card

--- src/components/CreditCardForm/CreditCardForm.tsx
+++ src/components/CreditCardForm/CreditCardForm.tsx
@@ -16,32 +16,58 @@ import './style.scss';
 const CreditCardForm = () => {
     const [isValid, handleValidate] = useState(false);
     const [showErrorMessage, handleErrorMessage] = useState(false);
-    const [creditCardNumber, handleCreditCardNumber] = useState('XXXX XXXX XXXX XXXX');
+    const [creditCardNumber, handleCreditCardNumber] = useState('•••• •••• •••• ••••');
     const [name, handleName] = useState('NAME');
-    const [creditCardValidationDate, handleCreditCardValidationDate] = useState('XX/XX');
+    const [creditCardValidationDate, handleCreditCardValidationDate] = useState('••/••');
     const [creditCardSecurityCode, handleCreditCardSecurityCode] = useState('');
     const [creditCardType, handleCreditCardType] = useState('initial');
+
+    const verifyCreditCardBand = number => {
+        const regexMastercard = new RegExp(/^(5[1-5]\d{4}|677189)\d{10}$/),
+            regexVisa = new RegExp(/^4\d{12}(\d{3})?$/),
+            regexAmericanExpress = new RegExp(/^3[47]\d{13}$/),
+            regexElo = new RegExp(/^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$/),
+            regexDinnersClub = new RegExp(/^3(0[0-5]|[68]\d)\d{11}$/),
+            regexDiscover = new RegExp(/^6(?:011|5[0-9]{2})[0-9]{12}$/);
+
+        if (regexMastercard.test(number)) return 'mastercard';
+        if (regexVisa.test(number)) return 'visa';
+        if (regexAmericanExpress.test(number)) return 'americanExpress';
+        if (regexElo.test(number)) return 'elo';
+        if (regexDinnersClub.test(number)) return 'dinnersClub';
+        if (regexDiscover.test(number)) return 'discover'
+        return 'initial';
+    }
 
     const validateCreditCard = event => {
         const number = event.target.value.replace(/\s/g, '');
 
         handleCreditCardNumber(event.target.value);
 
-        if (number.length === 16) {
+        if (number.length >= 14) {
             if (validateCardNumber(number)) {
                 handleValidate(true);
                 handleErrorMessage(false);
+
+                const creditCardBrand = verifyCreditCardBand(number);
+                handleCreditCardType(creditCardBrand);
             } else {
                 handleValidate(false);
                 handleErrorMessage(true);
+
+                handleCreditCardType('initial');
             }
         } else {
             handleValidate(false);
             handleErrorMessage(false);
+
+            handleCreditCardType('initial');
         } 
         
         if (number.length === 0) {
-            handleCreditCardNumber('XXXX XXXX XXXX XXXX');
+            handleCreditCardNumber('•••• •••• •••• ••••');
+
+            handleCreditCardType('initial');
         }
     }
 
@@ -64,6 +90,11 @@ const CreditCardForm = () => {
 
         return (sum % 10 === 0);
     }
+
+    const changeCreditCardClass = (toRemove, toAdd) => {
+        document.querySelector('.credit-card')?.classList.remove(toRemove);
+        document.querySelector('.credit-card')?.classList.add(toAdd);
+    }
   
     return (
       <FormSectionWrapper>
@@ -77,7 +108,7 @@ const CreditCardForm = () => {
 
             <Fieldset>
                 <Label>Name</Label>
-                <InputMask className="input" mask="" type="text" name="name" value={name} onChange={event => handleName(event.target.value)} />
+                <InputMask className="input" mask="" maskChar="" type="text" name="name" value={name} onChange={event => handleName(event.target.value)} />
             </Fieldset>
 
             <Fieldset>
@@ -87,7 +118,7 @@ const CreditCardForm = () => {
 
             <Fieldset>
                 <Label>Security </Label>
-                <InputMask className="input" mask="999" maskChar="" type="text" name="cardSecurityCode" value={creditCardSecurityCode} onChange={event => handleCreditCardSecurityCode(event.target.value)} />
+                <InputMask className="input" mask="999" maskChar="" type="text" name="cardSecurityCode" value={creditCardSecurityCode} onChange={event => handleCreditCardSecurityCode(event.target.value)} onFocus={() => changeCreditCardClass('front', 'back')} onBlur={() => changeCreditCardClass('back', 'front')} />
             </Fieldset>
 
             <Fieldset>
@@ -96,7 +127,10 @@ const CreditCardForm = () => {
             </Fieldset>
         </Form>
 
-        <CreditCard className={creditCardType}>
+        <CreditCard className={`credit-card front ${creditCardType}`}>
+            <span className="credit-card__item credit-card__chip"></span>
+            <span className={`credit-card__item credit-card__brand ${creditCardType}`}></span>
+            
             <span className="credit-card__item credit-card__number">{creditCardNumber}</span>
             <span className="credit-card__item credit-card__name">{name}</span>
             <span className="credit-card__item credit-card__validate-date">
@@ -105,8 +139,7 @@ const CreditCardForm = () => {
             </span>
             <span className="credit-card__item credit-card__security-code"> {creditCardSecurityCode}</span>
 
-            <span className="credit-card__item credit-card__chip"></span>
-            <span className="credit-card__item credit-card__brand"></span>
+            <span className={`credit-card__item credit-card__brand bottom ${creditCardType}`}></span>
         </CreditCard>
     </FormSectionWrapper>
   );
--- src/components/CreditCardForm/CreditCardFormStyle.tsx
+++ src/components/CreditCardForm/CreditCardFormStyle.tsx
@@ -50,9 +50,11 @@ const Button = styled.button`
 
 const CreditCard = styled.div`
     border-radius: 14px;
+    box-shadow: 0 1px 4px 0 rgba(0, 0, 0, .2);
     height: 180px;
     position: relative;
-    transition: all .3s ease-in-out;
+    transform-style: preserve-3d;
+    transition: background .3s ease-in-out, transform .4s linear;
     width: 290px;
 
     &.initial { background: linear-gradient(25deg,#999,#999); }
@@ -60,6 +62,51 @@ const CreditCard = styled.div`
     &.mastercard { background: linear-gradient(25deg,#f37b26,#fdb731); }
 
     &.visa { background: linear-gradient(25deg,#0f509e,#1399cd); }
+
+    &.americanExpress { background: linear-gradient(25deg,#308c67,#a3f2cf); }
+
+    &.elo { background: linear-gradient(25deg,#211c18,#aaa7a2); }
+    
+    &.dinnersClub { background: #FFF; }
+
+    &.discover { background: #0F0; }
+
+    .credit-card__item { opacity: 0; }
+
+    &.front {
+        .credit-card__number,
+        .credit-card__name,
+        .credit-card__validate-date,
+        .credit-card__brand,
+        .credit-card__chip { opacity: 1; }
+        
+        .credit-card__security-code { opacity: 0; }
+    }
+
+    &.back {
+        transform: rotateY(180deg);
+
+        .credit-card__number,
+        .credit-card__name,
+        .credit-card__validate-date,
+        .credit-card__chip { opacity: 0; }
+
+        .credit-card__brand.bottom {
+            bottom: 15%;
+            left: 50%;
+            right: initial;
+            top: initial;
+            transform: translateX(-50%);
+        }
+
+        .credit-card__brand.bottom,
+        .credit-card__security-code {
+            opacity: 1;
+            transform: rotateY(-180deg);
+        }
+
+        .credit-card__brand.bottom { transform: translateX(-50%) rotateY(-180deg); }
+    }
 `;
 
 const ErrorMsg = styled.span`
